2b42c1582df10dd7c1d309bffc51358c3674cb74
dfhissd <429246362@qq.com>
2019-07-12 09:23:24 +0800

    创建线程

--- MulityThreadNote/Program.cs
+++ MulityThreadNote/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 
 namespace MulityThreadNote
 {
@@ -9,7 +10,34 @@ namespace MulityThreadNote
     {
         static void Main(string[] args)
         {
+            //=====================创建线程======================//
+            Thread t1 = new Thread(new ThreadStart(PrintNumbers));//无参数的委托
+            t1.Start();
+
+            Thread t2 = new Thread(new ParameterizedThreadStart(PrintNumbers));//有参数的委托
+            t2.Start(10);
+            Console.ReadKey();
+            
 
         }
+        //=====================创建线程======================//
+        static void PrintNumbers()
+        {
+            Console.WriteLine("PrintNumbers Starting...");
+            for (int i = 0; i < 10; i++)
+                Console.Write(i);
+            Console.WriteLine();
+        }
+        //注意：要使用ParameterizedThreadStart，定义的参数必须为object
+        static void PrintNumbers(object count)
+        {
+            Console.WriteLine("PrintNumbers Starting...");
+            for (int i = 0; i < Convert.ToInt32(count); i++)
+                Console.Write(i);
+            Console.WriteLine();
+        }
+
+
+
     }
 }
